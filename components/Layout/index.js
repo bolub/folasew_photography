5e42efe8d40c32a45b4d5be3cf714e6898c66791
bolub <abiol5202@gmail.com>
2021-02-06 18:59:41 +0100

    add meta tags

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -17,6 +17,24 @@ const Layout = ({ children, title, getSingleImage }) => {
     <>
       <Head>
         <title>{title} &bull; Folasewa Photography</title>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicon/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/favicon/site.webmanifest" />
         {/* <meta
           name="description"
           content="Web design company located in Lagos. We also provide Branding, Search Engine Optimization, Digital Marketing, UI/UX designs, Mobile app development services."
