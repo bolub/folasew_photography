8bed1dd02bc1553cc3bf9f128bb677ff99f3482b
bolub <abiol5202@gmail.com>
2021-02-06 13:09:48 +0100

    fix the clipRule issue

--- components/UI/Navbar/MobileView.js
+++ components/UI/Navbar/MobileView.js
@@ -49,7 +49,7 @@ const MobileView = ({ setToShow, aboutUsbtnRef, gd, gr }) => {
                 <path
                   fillRule="evenodd"
                   d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z"
-                  clip-rule="evenodd"
+                  clipRule="evenodd"
                 />
               </svg>
             </Text>
